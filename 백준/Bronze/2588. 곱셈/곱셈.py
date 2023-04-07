d94900c054fe336091c96fc4055efc7af833920d
김상주 <rlatkdwn3379@gmail.com>
2023-04-07 14:55:18 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+import sys
+a = int(sys.stdin.readline())
+b = sys.stdin.readline()
+
+axb1 = a*int(b[2])
+axb2 = a*int(b[1])
+axb3 = a*int(b[0])
+axb = a*int(b)
+print(axb1)
+print(axb2)
+print(axb3)
+print(axb)
