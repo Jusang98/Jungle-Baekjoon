879664bca765e0690448cab73bcbd55affa3779a
김상주 <rlatkdwn3379@gmail.com>
2023-04-07 15:04:19 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+import sys
+a = int(sys.stdin.readline())
+if a >= 90:
+    print("A")
+if a >=80 and a<90:
+    print("B")
+if a>=70 and a<80:
+    print("C")
+if a>=60 and a<70:
+    print("D")
+if a<60:
+    print("F")
+
