0d625d17381d854957968768fd094c4aebea40fb
김상주 <rlatkdwn3379@gmail.com>
2023-04-08 22:09:16 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -5,13 +5,18 @@ n = int(sys.stdin.readline())
 nums = map(int, sys.stdin.readline().split())
 count = 0
 
-for i in nums:
-    if i > 1:
-        noCount = 0
-        for j in range(2, i):
 
-            if i % j == 0:
-                noCount += 1
-        if noCount == 0:
-            count += 1
+# i 3일떄 계산
+def prime(a):
+    if a < 2:
+        return False
+    for i in range(2, a):
+        if a % i == 0:
+            return False
+    return True
+
+
+for j in nums:
+    if prime(j):
+        count += 1
 print(count)
