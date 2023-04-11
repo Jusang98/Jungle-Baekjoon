fef20ea025d4a85b5ae25b71b06dc6bab88c946d
김상주 <rlatkdwn3379@gmail.com>
2023-04-11 14:04:38 +0900

    [Silver I] Title: 하노이 탑, Time: 504 ms, Memory: 153812 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,20 +1,19 @@
 import sys
 
-# n: 원판 갯수 , s기둥, m 기둥, e 기둥
 n = int(sys.stdin.readline())
 
 
+# n: 원판 갯수 , 1번기둥(start) - s기둥, 2번 기둥(middle) - m 기둥, 3번 기둥(end) - e 기둥
 def hanoi(n, s, e):
-    m = 6 - s - e
+    m = 6 - s - e  # 2번기둥
     if n == 1:
-        print(s, e)
-        return
+        print(s, e) #
     else:
-        hanoi(n - 1, s, m)
-        print(s, e)
-        hanoi(n - 1, m, e)
+        hanoi(n - 1, s, m) # n-1 의 원판을 전부 s -> m (그 안에서 print 1,3 -> 1,2 ->  3,2...)
+        print(s, e) #남은 하나를 s -> e 1,3
+        hanoi(n - 1, m, e) # m에 위치한 n-1의 원판을 전부 -> e로
 
 
-print(2 ** n - 1)
-if n <= 20:
+print(2 ** n - 1) # n개의 판 이동횟수 구하는 식
+if n <= 20: # 조건
     hanoi(n, 1, 3)
