b6fa823a88edbe4fe59e94a7e604dcf67ff0ec56
김상주 <rlatkdwn3379@gmail.com>
2023-04-11 10:45:46 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,14 +1,12 @@
 import sys
 
-n = int(sys.stdin.readline())
+N = int(sys.stdin.readline())
 cnt = 0
-for i in range(n+1):
-    if i == 0:
-        cnt -= 1
+for i in range(1, N + 1):  
     if i < 100:
         cnt += 1
     else:
-        a = list(str(i))
-        if int(a[1]) - int(a[0]) == int(a[2]) - int(a[1]):
+        a = list(str(i))  
+        if int(a[1]) - int(a[0]) == int(a[2]) - int(a[1]):  
             cnt += 1
 print(cnt)
