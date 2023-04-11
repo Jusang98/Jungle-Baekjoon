097d6f80e9f8374752e97d815c5d7cf88a0c4e79
김상주 <rlatkdwn3379@gmail.com>
2023-04-11 09:16:58 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -3,9 +3,9 @@ import math
 
 condition = list(map(int, sys.stdin.readline().split()))
 
-A = condition[0]  # 달팽이가 낮에 올라갈 높이 ex) input(2)
-B = condition[1]  # 달팽이가 밤에 내려올 높이 ex) input(1)
-V = condition[2]  # V높이의 나뭇가지 ex) input(5)
-days = (V - B) / (A - B) # 정상에 올라가면 달팽이는 내려오지않는다.
-# ex) V/(A-B)를 할시 5/1 5일이 걸린다고 출력되지만 실제론 4일이 걸린다. 따라서 V에서 B를 미리 뺴줌으로서
-print(math.ceil(days))  # math.ceil로 나머지가 남으면 올림 즉 아직 달팽이가 갈 길이가 남아있다면 day 올림
+A = condition[0] 
+B = condition[1] 
+V = condition[2] 
+days = (V - B) / (A - B)  
+
+print(math.ceil(days)) 
