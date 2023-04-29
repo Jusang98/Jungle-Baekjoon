e55bd01e2ae2aea95d51a7a55d1ae6ae752d24ec
김상주 <rlatkdwn3379@gmail.com>
2023-04-29 21:24:40 +0900

    [Gold V] Title: LCS, Time: 580 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,14 @@
+import sys
+
+
+word1, word2 = sys.stdin.readline().strip(), sys.stdin.readline().strip()
+h, w = len(word1), len(word2)
+cache = [[0] * (w+1) for _ in range(h+1)]
+
+for i in range(1, h+1):
+    for j in range(1, w+1):
+        if word1[i-1] == word2[j-1]:
+            cache[i][j] = cache[i-1][j-1] + 1
+        else:
+            cache[i][j] = max(cache[i][j-1], cache[i-1][j])
+print(cache[-1][-1])
