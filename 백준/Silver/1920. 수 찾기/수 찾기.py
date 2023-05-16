c977514bb5f12493f7e0af4f4fe4a59944cab41e
김상주 <rlatkdwn3379@gmail.com>
2023-05-16 16:34:37 +0900

    [Silver IV] Title: 수 찾기, Time: 288 ms, Memory: 139992 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,12 +1,27 @@
 import sys
 
 N = int(sys.stdin.readline())
-A = set(map(int, sys.stdin.readline().split()))
-M = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().split()))
+numlist = list(map(int, sys.stdin.readline().split()))
+X = int(sys.stdin.readline())
+findlist = list(map(int, sys.stdin.readline().split()))
 
-for i in a:
-    if i in A:
+numlist.sort()
+
+
+def binarysearch(num):
+    left = 0
+    right = N - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if num == numlist[mid]:
+            return True
+        elif num < numlist[mid]:
+            right = mid - 1
+        elif num > numlist[mid]:
+            left = mid + 1
+
+for i in findlist:
+    if binarysearch(i):
         print(1)
     else:
         print(0)
